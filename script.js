49b24f1826753a968ebdf3fbd853198810cc79c5
Jacob San <>
2023-01-30 16:29:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" 
+  href="style.css" />
+
+<html>
+
+<body>
+  <nav>
+<a href="index.html"> <button class="buttons" type="button">back</button></a>
+    
+    <a href="IMAge2.html"> <button class="buttons" type="button">next</button></a>
+
+  </nav>
+
+  <div class="IMAge2">
+    <h1>wow look at these animals!</h1>
+
+    <img class="IMAge2" src="https://images.unsplash.com/photo-1525869916826-972885c91c1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=100"
+  
+</body>
+
+  
+</html>
